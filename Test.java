bb7f8520d739b47352df56ad9ccf05fc01c07707
Nick Zimanski <nick.zimanski@gmail.com>
2021-08-30 16:19:27 -0400

    Initial commit

---
+++ Test.java
@@ -0,0 +1,16 @@
+public class Test {
+    public static void main(String[] args) {
+        GarbageFire gf = new GarbageFire();
+        gf.GarbonzoBeans();
+    }
+}
+
+class GarbageFire {
+    public GarbageFire() {
+
+    }
+
+    public void GarbonzoBeans() {
+        System.out.println("beans");
+    }
+}

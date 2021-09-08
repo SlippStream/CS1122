b9065c7a8cb8a552f75779eabd510f011832191b
Nick Zimanski <nick.zimanski@gmail.com>
2021-09-08 10:51:17 -0400

    9/8/2021 10:51

--- Week2/FileStats.java
+++ Week2/FileStats.java
@@ -7,31 +7,41 @@ import java.util.Scanner;
  * @author Nick Zimanski
  * @class CS 1122
  * @lab L01-J
- * @version 9/7/2021
+ * @version 9/8/2021
  */
 public class FileStats {
     public static void main(String[] args) {
         Scanner userInput = new Scanner(System.in);
+        char character;
         String line;
         String[] tokenWords, tokenChars;
         int characters = 0, lines = 0, words = 0;
+
         while(true) {
             System.out.print("Enter a filename: ");
             String filename = userInput.nextLine();
             File inFile = new File(filename);
-            try {
-                Scanner fileInput = new Scanner(inFile);
+
+            try(Scanner fileInput = new Scanner(inFile)) {
+                fileInput.useDelimiter("");
                 while (fileInput.hasNextLine()) {
                     line = fileInput.nextLine();
                     tokenWords = line.split(" ");
-                    tokenChars = line.split("");
 
-                    characters += tokenChars.length;
+                    Scanner lineScanner = new Scanner(line);
+                    lineScanner.useDelimiter("");
+                    while (lineScanner.hasNext()) {
+                        character = lineScanner.next().charAt(0);
+                        characters++;
+                        System.out.print(character);
+                    }
+                    lineScanner.close();
+
                     words += tokenWords.length;
                     lines++;
-                    characters++; //Count the newline
+                    characters++;
                 }
-                if (characters > 0) characters--; //Don't count the last line as a newline
+                if (characters > 0) characters--;
 
                 System.out.println("characters = " + characters);
                 System.out.println("words = " + words);
